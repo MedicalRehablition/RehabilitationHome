b2e0543c18bb62c66fafaacc84c2e4eb99cfc9e3
gimmywu <girl5380@hotmail.com>
2023-01-03 05:35:00 +0800

    會談介面

---
+++ prjRehabilitation/Controllers/ConsultController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prjRehabilitation.Models;
+using prjRehabilitation.ViewModel;
+using System.Xml.Linq;
+
+namespace prjRehabilitation.Controllers
+{
+    public class ConsultController : Controller
+    {
+        public IActionResult List(CKeywordViewModel vm)
+        {
+            dbClassContext db = new dbClassContext();
+            string Keyword = vm.txtKeyword;
+            IEnumerable<PatientInfo> data = null;
+            if (Keyword == null)
+            {
+                data = from p in db.PatientInfos
+                       select new PatientInfo { FName = p.FName };
+            }
+            else
+                data = db.PatientInfos.Where(c => c.FName.Contains(Keyword)).ToList();
+            List<CPatientsViewModel> List = new List<CPatientsViewModel>();
+            foreach (var c in data)
+            {
+                CPatientsViewModel patient = new CPatientsViewModel();
+                patient.Patient = c;
+                List.Add(patient);
+            }
+            return View(List);         
+        }
+        //public IActionResult DateList()
+        //{
+        //}
+            
+
+
+
+
+    }
+}
