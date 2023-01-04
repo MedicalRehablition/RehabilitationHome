c8cc12d653c727ce19d07dec099d2625a79dfbd9
ChrisYee8787 <117145269+ChrisYee8787@users.noreply.github.com>
2023-01-04 12:54:31 +0800

    更新資料庫

---
+++ prjRehabilitation/Controllers/UserLoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjRehabilitation.Controllers
+{
+    public class UserLoginController : Controller
+    {
+        public IActionResult index()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ prjRehabilitation/Models/Admin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace prjRehabilitation.Models
+{
+    public partial class Admin
+    {
+        public int Fid { get; set; }
+        public string? FName { get; set; }
+        public string? FEmail { get; set; }
+        public string? FPassword { get; set; }
+        public string? FRank { get; set; }
+    }
+}
