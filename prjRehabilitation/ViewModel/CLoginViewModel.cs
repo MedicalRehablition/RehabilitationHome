91879fec6dd07e370b8e439aaaee763a943b748e
ChrisYee8787 <yeeee880726@gmail.com>
2023-01-10 13:50:17 +0800

    套版90%有些東西看起來很奇怪的我有註解

--- prjRehabilitation/ViewModel/CLoginViewModel.cs
+++ prjRehabilitation/ViewModel/CLoginViewModel.cs
@@ -5,7 +5,6 @@ namespace prjRehabilitation.ViewModel
     public class CLoginViewModel
     {
         public string txtAccount { get; set; }
-       
         public string txtPassword { get; set; }
     }
 }
