c613c7ea7c09ec9c62ecf3c95073150e800ed54a
eric <ericispanasp@gmail.com>
2023-01-16 03:47:32 +0800

    1

---
+++ prjRehabilitation/ViewModel/Eric/CGroupActivityEditViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace prjRehabilitation.ViewModel.Eric
+{
+    public class CGroupActivityEditViewModel
+    {
+        public IEnumerable<string>? EventType { get; set; }
+
+        public CGroupActivityViewModel? cgavm { get; set; }
+        public List<string>? GroupActivityType
+        {
+            get
+            {
+                return new List<string>
+                {
+                    "體能",
+                    "烹飪",
+                    "護理衛教",
+                    "家事清潔",
+                    "財務管理",
+                    "休閒娛樂",
+                    "社區適應討論會",
+                    "住民自治會議"
+                };
+            }
+        }
+    }
+}
