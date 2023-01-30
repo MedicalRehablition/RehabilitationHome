85f03dd479dd3e8cc85826cc108b396aecb0a2fb
gimmywu <113874913+gimmywu@users.noreply.github.com>
2023-01-31 05:32:43 +0800

    新增qrcode掃描功能，下載ZXing及ZXing.Net Bindings.Windows Compatibilitynuget套件

---
+++ prjRehabilitation/Controllers/WebcamController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+using System.Net.NetworkInformation;
+
+namespace prjRehabilitation.Controllers
+{
+    public class WebcamController : Controller
+    {
+        public IActionResult Camera()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult decode(string imageData)
+        {
+            //IBarcodeReader reader = new BarcodeReader();
+            var reader = new ZXing.Windows.Compatibility.BarcodeReader();
+            //BarcodeReader r = new BarcodeReader();
+            if (imageData != null)
+            {
+                var image = (Bitmap)new ImageConverter().ConvertFrom(Convert.FromBase64String(imageData.Split(',')[1]));
+                //var image = (Bitmap)Image.FromFile("D:\\C#\\QRcode\\qrcodePractice\\qrcodeform\\images\\ZxingQrcode.png");//測試
+                //var image = (Bitmap)Image.FromFile("D:\\github\\期末測試用\\RehabilitationHome-gimmywu-patch-1\\prjRehabilitation\\wwwroot\\images\\ZxingQrcodewithbackground.png");//圖片有背景測試
+                //byte[] image = Convert.FromBase64String(imageData.Split(',')[1]);
+                var result = reader.Decode(image);
+                if (result != null)
+                {
+                    string decodedText = result.Text;
+                    return Json(new { decodedText });
+                }
+            }
+            return Json(new { decodedText = "no pic" });
+
+        }
+
+    }
+}
