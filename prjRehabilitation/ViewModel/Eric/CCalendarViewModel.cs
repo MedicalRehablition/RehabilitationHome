0e81235d775ccd105665cf3b64f32c259861d6f2
ericCYG <ericIspanAsp@gmail.com>
2023-02-02 20:55:28 +0800

    加了一半的新建行事曆

---
+++ prjRehabilitation/ViewModel/Eric/CCalendarViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace prjRehabilitation.ViewModel.Eric
+{
+	public class CCalendarViewModel
+	{
+		[DisplayName("設定日期")]
+		[Required(ErrorMessage = "必填")]
+		public string? date { get; set; }
+		[DisplayName("行事曆提示字")]
+		[Required(ErrorMessage = "必填")]
+		public string? eventName { get; set; }
+	
+		public string? className { get; set; }
+	
+		public string? dateColor { get; set; }
+		[DisplayName("行事曆抬頭")]
+		[Required(ErrorMessage = "必填")]
+		public string? title { get; set; }
+		[DisplayName("行事曆內容")]
+		[Required(ErrorMessage = "必填")]
+		public string? content { get; set; }
+		[DisplayName("填表人")]
+		[Required(ErrorMessage = "必填")]
+		public string? fRecorder { get; set; }
+		[DisplayName("填表日期")]
+		[Required(ErrorMessage = "必填")]
+		public string? fRecorderDate { get; set; }
+	}
+}
