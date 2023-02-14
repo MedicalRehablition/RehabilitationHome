261b8c9eaa5daac3c1c4803d77e5468f41cea5d7
eric <ericispanasp@gmail.com>
2023-02-15 03:35:52 +0800

    行事曆人名，延展日，上一月下一月改完

--- prjRehabilitation/ViewModel/Eric/CCalendarTotalNeedViewModel.cs
+++ prjRehabilitation/ViewModel/Eric/CCalendarTotalNeedViewModel.cs
@@ -5,7 +5,7 @@ namespace prjRehabilitation.ViewModel.Eric
     public class CCalendarTotalNeedViewModel
     {
         public List<Consultation> ResidentReVisitDay { get; set; }
-
+        public List<PatientInfo> ResidentExpireDate { get; set; }
         public List<TCalendar> GetTodayNextAndFrontOneMonth { get; set; }
 
         public string getFrontSession { get; set; }
